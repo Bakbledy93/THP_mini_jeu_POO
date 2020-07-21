e47e3e530eb4111b413606d727f0fa85e2f382bd
bakbledy93 <you@example.com>
2020-07-21 22:42:24 +0200

    add comments

--- app_2.rb
+++ app_2.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-require_relative 'lib/game'
+#require_relative 'lib/game'
 require_relative 'lib/player'
 
 puts "-----------------------------------------------------"
@@ -10,46 +10,54 @@ puts "|Kill all the cows! Now, get a mooooove on!         |"
 puts "-----------------------------------------------------"
 
 
-#Joueur Humain
+#Joueur Humain -  Initilisation du joueur HumanPlayer
 puts "What's your name cowboy ?"
 player_name = gets.chomp
 player1 = HumanPlayer.new(player_name)
 
 print "> Howdy, #{player1.name}!!\n\n"
 
-#NPCs
+#NPCs - Initialisation des NPCs
 player2 = Player.new("Vache 1")
 player3 = Player.new("Vache 2")
 ennemies = [player2, player3]
 
 #MENU
 def menu(player1, player2, player3)
+  puts player1.show_state
   puts "C'est votre tour #{player1.name}. Que voulez-vous faire ?"
   puts "a - chercher une meilleure arme"
   puts "s - chercher à se soigner"
+  # Si les points de vie du  NPC 2 sont nuls, il n'apparait plus dans le menu
   if player2.life_points > 1
     puts "1 - pour attaquer Hector the bull qui à #{player2.life_points} points de vie"
   end
+    # Si les points de vie du  NPC 3 sont nuls, il n'apparait plus dans le menu
   if player3.life_points > 1
     puts "2 - pour attaquer Dolly the cow qui à #{player3.life_points} points de vie"
   end
   print "> "
- choice = gets.chomp
- case choice
- when 'a'
-  player1.search_weapon
- when 's'
-  player1.search_health_pack
- when '1'
-  player1.attacks(player2)
- when '2'
-   player1.attacks(player3)
- else
-   puts "Oups."
- end
+
+  choice = gets.chomp
+
+  # la fonction case permet de faire fonctionner le menu
+  case choice
+    when 'a'
+      player1.search_weapon
+    when 's'
+      player1.search_health_pack
+    when '1'
+      player1.attacks(player2)
+    when '2'
+      player1.attacks(player3)
+    else
+      puts "Oups. T'as quand même énervé les vaches.\n\n"
+  end
+
 end
 
 # COMBAT !
+# la fonction while est initialisé de tel sorte qu'après chaque tour, on repart à partir du menu
 while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points > 0)
   menu(player1, player2, player3)
 
@@ -59,8 +67,10 @@ while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points
       ennemy.attacks(player1)
     end
   end
+
 end
 
+#FIN DE PARTIE
 puts "La partie est finie"
 if player1.life_points > 0
   puts "You're a real cowboy YEEEEHAAAAAA !"
--- lib/player.rb
+++ lib/player.rb
@@ -1,89 +1,105 @@
 require 'pry'
 
+# Initilisation de la classe Player
 class Player
   attr_accessor :name, :life_points
   
+  #Permet d'initialiser les joueurs
   def initialize(player_name)
     @name = player_name.to_s
     @life_points = 10
   end
 
+  #Affiche les points de vie d'un joueur
   def show_state
     puts "#{@name} à #{@life_points} points de vie"
   end
 
+  #Inflige des dommages à un joueur
   def gets_damage(damage)
     @life_points = @life_points - damage
   end
-	
+  
+  #Détermine les points de dommages à infliger dans la méthode gets_damage
 	def compute_damage
     return rand(1..6)
   end
 
+  #Initialise la fonction d'attaque
   def attacks(player)
 		puts "#{@name} attaque #{player.name} !"
 		damage = compute_damage
     player.gets_damage(damage)
     
 		if player.life_points > 0
-		puts "#{@name} lui inflige #{damage} points de dommage."
+		puts "#{@name} lui inflige #{damage} points de dommage.\n\n"
 		else 
-			puts "Oups, #{player.name} c'est fait décapité..."
+			puts "Oups, #{player.name} c'est fait décapité...\n\n"
     end
 
   end
     
 end 
 
+#Initialisation de la classe HumanPlayer
 class	HumanPlayer < Player
 	attr_accessor :weapon_level
 
   def initialize(player_name)
-    super(player_name)
+    super(player_name) #héritage de la classe Player
     @life_points = 100
     @weapon_level = 1 
   end
 
+  #Fonction supplémentaire de la méthode show_state
   def show_state
-    puts "#{@name} à #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+    puts "#{@name} à #{@life_points} points de vie et une arme de niveau #{@weapon_level} \n\n"
   end
 
+  #Fonction supplémentaire de la méthode compute_damage
   def compute_damage
     rand(1..6) * @weapon_level
   end
 
+  #Méthode search_weapon pour la recherche d'une nouvelle arme
   def search_weapon
     new_weapon_level = rand(1..6)
     puts new_weapon_level
 
       if  new_weapon_level > @weapon_level 
         @weapon_level = new_weapon_level
-        puts "ok tu as trouvé une super vache tueuse de niveau #{new_weapon_level}"
+        puts "ok tu as trouvé une super vache tueuse de niveau #{new_weapon_level}\n\n"
       else
-        puts "on la laisse par terre car c'était un petit veau de niveau mouuuuh #{new_weapon_level}"
+        puts "on la laisse par terre car c'était un petit veau de niveau mouuuuh #{new_weapon_level}\n\n"
       end
   end
 
+  #Méthode search_health_pack pour la recherche de points de vie
   def search_health_pack
     dice_health_pack = rand(1..6)
     if dice_health_pack == 1
       puts "Tu n'as rien trouvé malheureux.."
+
     elsif dice_health_pack >= 2 && dice_health_pack <= 5
       @life_points += 50
+
       if @life_points > 100
         @life_points = 100
-        puts "T'es déjà au max mon gars, laisse un peu aux autres"
+        puts "T'es déjà au max mon gars, laisse un peu aux autres" #Mets la limite de points de vie max à 100
       else
         puts "T'as choppé du Redbull qui te redonnes 50 points de vie, tu as #{@life_points} points de vie"
       end
+
     elsif
       @life_points += 80
+
       if @life_points > 100
         @life_points = 100
-        puts "T'es déjà au max mon gars, laisse un peu aux autres"
+        puts "T'es déjà au max mon gars, laisse un peu aux autres" #Mets la limite de points de vie max à 100
       else
         puts "T'as choppé un mega Redbull qui te redonnes 80 points de vie, tu as #{@life_points} points de vie"
       end
+
     end
   end
 
