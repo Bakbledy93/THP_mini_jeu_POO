c8532f19beb0c4836313668950c8966742a12dc7
DavidBeraud <dberaud@gmail.com>
2020-07-21 19:07:11 +0200

    modif David v2.0

--- app_2.rb
+++ app_2.rb
@@ -18,95 +18,57 @@ player1 = HumanPlayer.new(player_name)
 print "> Howdy, #{player1.name}!!\n\n"
 
 #NPCs
-player2 = Player.new("Hector the bull")
-player3 = Player.new("Dolly the cow")
+player2 = Player.new("Vache 1")
+player3 = Player.new("Vache 2")
 ennemies = [player2, player3]
 
-#combat
-puts "Let's get ready to rumble!!\n\n"
-
-while player1.life_points >0 && (player2.life_points > 0 || player3.life_points >0)
-puts "\n\n"
-puts "A toi de jouer #{player1.name}, tu veux ?"    #menu options
-puts "---------------------------"
-puts "a - tester une nouvelle arme"
-puts "s - se soigner"
-puts "\n\n"
-puts "Attaquer une vache en vue :"
-puts "---------------------------"
-puts "0 - pour attaquer Hector the bull qui à #{player2.life_points} points de vie"
-puts "1 - pour attaquer Dolly the cow qui à #{player3.life_points} points de vie"
-puts "\n\n"
-decision = gets.chomp.to_s    # menu
-if decision == "a" 
-  decision = player1.search_weapon  #si le jouer décide de chercher une meilleur arme
-
-elsif decision == "s"
- decision = player1.search_health_pack  #si le jouer décide de chercher un health pack
-
- elsif decision == "0" 
-  # decision = player1.attacks(player2)   #si le jouer décide d'attaquer Hector the bull
-
- elsif decision == "1"
-  # decision = player1.attacks(player3)   #si le jouer décide d'attaquer Dolly the cow
- end
-
-
-  if decision == "0" || decision =="1"   # execution de l'attaque
-    if decision =="0"
-      if player1.life_points > 0  && player2.life_points > 0
-      decision = player1.attacks(player2) 
-      puts player1.show_state
-      end
-
-      if player2.life_points <= 0 && player3.life_points <= 0   # si les deux vaches meurent
-        break
-      end
-
-      if player2.life_points > 0 && player1.life_points > 0    
-      player2.attacks(player1)
-      puts player1.show_state
-      end
-
-      if player1.life_points <= 0 
-        break
-      end
-
-    elsif decision =="1"
-
-      if  player1.life_points > 0 && player3.life_points >0
-        decision = player1.attacks(player3)
-        puts player1.show_state
-      end
-
-      if player3.life_points <= 0 && player2.life_points <= 0 
-        break
-      end
-
-      if player3.life_points > 0 && player1.life_points > 0
-      player3.attacks(player1)
-      puts player1.show_state
-      end
-
-      if player1.life_points <= 0 
-        break
-      end
-    end
+#MENU
+def menu(player1, player2, player3)
+  puts "C'est votre tour #{player1.name}. Que voulez-vous faire ?"
+  puts "a - chercher une meilleure arme"
+  puts "s - chercher à se soigner"
+  if player2.life_points > 0
+    puts "1 - Attaquer #{player2.name} (#{player2.life_points} pv)"
   end
-### Here the cows attack
-
-
-
+  if player3.life_points > 0
+    puts "2 - Attaquer #{player3.name} (#{player3.life_points} pv)"
+  end
+  print "> "
+ choice = gets.chomp
+ case choice
+ when 'a'
+  player1.search_weapon
+ when 's'
+  player1.search_health_pack
+ when '1'
+  player1.attacks(player2)
+ when '2'
+   player1.attacks(player3)
+ else
+   puts "Oups."
+ end
 end
 
-if player1.life_points >= 0 && player2.life_points <= 0 && player3.life_points <= 0 
-  puts "You're a real cowboy YEEEEHAAAAAA ! "
+# COMBAT !
+while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points > 0)
+  menu(player1, player2, player3)
+
+  ennemies.each do |ennemy|
+    if ennemy.life_points >= 0
+      puts "Les vaches vous attaquent !"
+      ennemy.attacks(player1)
+    end
+  end
+end
+
+puts "La partie est finie"
+if player1.life_points > 0
+  puts "You're a real cowboy YEEEEHAAAAAA !"
 else
   puts "#{player1.name} has 0 life points remaining."
   puts "you have lost, looooser!"
 end
 
 
- 
 binding.pry
 puts "fin du game" 
